109304bc6ef11769b18bb2f8741259b111c7ee09
Fuad Rabbi <fuadhrabbi66@gmail.com>
2026-01-01 12:40:02 -0500

    refactor BookService and BookServiceImpl to include BookException handling and update method signatures

--- src/main/java/com/rabbi/services/BookService.java
+++ src/main/java/com/rabbi/services/BookService.java
@@ -1,5 +1,6 @@
 package com.rabbi.services;
 
+import com.rabbi.exception.BookException;
 import com.rabbi.payload.dto.BookDTO;
 import com.rabbi.payload.request.BookSearchRequest;
 import com.rabbi.payload.response.PagesResponse;
@@ -8,9 +9,9 @@ import java.util.List;
 
 public interface BookService {
 
-    BookDTO createBook(BookDTO bookDTO);
-    List<BookDTO> createBooksBulk();
-    BookDTO getBookById(Long bookId);
+    BookDTO createBook(BookDTO bookDTO) throws BookException;
+    List<BookDTO> createBooksBulk(List<BookDTO> bookDTOs) throws BookException;
+    BookDTO getBookById(Long bookId) throws BookException;
     BookDTO getBooksByISBN(String isbn);
     BookDTO updateBook(Long bookId, BookDTO bookDTO);
     void deleteBook(Long bookId);
--- src/main/java/com/rabbi/services/impl/BookServiceImpl.java
+++ src/main/java/com/rabbi/services/impl/BookServiceImpl.java
@@ -1,30 +1,57 @@
 package com.rabbi.services.impl;
 
+import com.rabbi.exception.BookException;
+import com.rabbi.mapper.BookMapper;
+import com.rabbi.model.Book;
 import com.rabbi.payload.dto.BookDTO;
 import com.rabbi.payload.request.BookSearchRequest;
 import com.rabbi.payload.response.PagesResponse;
+import com.rabbi.repo.BookRepository;
 import com.rabbi.services.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
+
     @Override
-    public BookDTO createBook(BookDTO bookDTO) {
-        return null;
+    public BookDTO createBook(BookDTO bookDTO) throws BookException {
+        if(bookRepository.existsByIsbn(bookDTO.getIsbn())){
+            throw new BookException("Book with ISBN already exists: " + bookDTO.getIsbn());
+        }
+
+        Book book = bookMapper.toEntity(bookDTO);
+
+        book.isAvailableCopiesValid();
+        Book savedBook = bookRepository.save(book);
+
+        return bookMapper.toDTO(savedBook);
     }
 
     @Override
-    public List<BookDTO> createBooksBulk() {
-        return List.of();
+    public List<BookDTO> createBooksBulk(List<BookDTO> bookDTOs) throws BookException {
+
+        List<BookDTO> createdBooks = new ArrayList<>();
+        for(BookDTO bookDTO:bookDTOs) {
+            BookDTO book = createBook(bookDTO);
+            createdBooks.add(book);
+        }
+        return createdBooks;
     }
 
     @Override
-    public BookDTO getBookById(Long bookId) {
-        return null;
+    public BookDTO getBookById(Long bookId) throws BookException {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new BookException("Book not found with id: " + bookId));
+        return bookMapper.toDTO(book);
+
     }
 
     @Override
