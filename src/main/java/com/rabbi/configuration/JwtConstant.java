13ea9091d629baf4c9d44b9b4840d5ec8673c345
Fuad Rabbi <fuadhrabbi66@gmail.com>
2026-01-03 11:44:56 -0500

    add JWT authentication support with JwtConstant, JwtValidator, and SecurityConfig

---
+++ src/main/java/com/rabbi/configuration/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.rabbi.configuration;
+
+public class JwtConstant {
+    public static final String JWT_HEADER = "Authorization";
+    public static final String SECRET_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30";
+}
