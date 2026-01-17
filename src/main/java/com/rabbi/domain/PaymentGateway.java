60b51e20c8cc52550462345f1f7fd27811dd250a
Fuad Rabbi <fuadhrabbi66@gmail.com>
2026-01-17 11:48:18 -0500

    Add `Payment` model, enums, and related fields
    Introduce `Payment` model with attributes and JPA annotations. Add `PaymentGateway`, `PaymentStatus`, and `PaymentType` enums for improved transaction handling and categorization.

---
+++ src/main/java/com/rabbi/domain/PaymentGateway.java
@@ -0,0 +1,6 @@
+package com.rabbi.domain;
+
+public enum PaymentGateway {
+    RAZORPAY,
+    STRIPE
+}
---
+++ src/main/java/com/rabbi/domain/PaymentStatus.java
@@ -0,0 +1,8 @@
+package com.rabbi.domain;
+
+public enum PaymentStatus {
+    PENDING,
+    SUCCESS,
+    FAILED,
+    CANCELLED
+}
